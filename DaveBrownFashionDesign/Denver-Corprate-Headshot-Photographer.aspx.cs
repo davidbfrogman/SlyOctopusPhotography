285c393411de4c5509a3b8a75b12cba7b3e369f5
davidbfrogman <info@davebrownphotography.com>
2014-04-17 10:35:36 -0600

    One more double check test
    One more double check test

--- DaveBrownFashionDesign/Denver-Corprate-Headshot-Photographer.aspx.cs
+++ DaveBrownFashionDesign/Denver-Corprate-Headshot-Photographer.aspx.cs
@@ -18,7 +18,7 @@ namespace DaveBrownPhotography
 {
     public partial class CorprateHeadshot_Photography : PortfolioBasePage
     {
-        //adding for test
+        //adding for testing github
         public override string ImageDirectory { get{return "Portraits";}}
         public override HtmlGenericControl imageHolderControl { get { return this.ulImageHolder; } }
         public override HtmlGenericControl BlogHolder { get { return this.divBlogHolder; } }
